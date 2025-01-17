29fea51a8e77390d8955c6cc11f26d5b09350e60
Louai-Souei <louai.souei04@gmail.com>
2025-01-18 00:07:26 +0100

    jenkins update''

---
+++ server/chaging-file.js
@@ -0,0 +1,22 @@
+// app.js
+function greetUser(name) {
+    if (!name) {
+        return "Hello, Guest!";
+    }
+    return `Hello, ${name}!`;
+}
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Export functions for testing
+module.exports = {
+    greetUser,
+    addNumbers,
+    multiplyNumbers,
+};
